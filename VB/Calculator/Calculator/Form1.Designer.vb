b8a8d19a46aaa923e9bc83475b5030be1252a934
Ryan Conroy <rjcon597@learner.dvusd.org>
2022-09-07 13:18:58 -0700

    Calculator Program Files
    Contains code for a Calculator

---
+++ VB/Calculator/Calculator/Form1.Designer.vb
@@ -0,0 +1,350 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DisplayTextBox = New System.Windows.Forms.TextBox()
+        Me.B7 = New System.Windows.Forms.Button()
+        Me.B9 = New System.Windows.Forms.Button()
+        Me.B8 = New System.Windows.Forms.Button()
+        Me.B5 = New System.Windows.Forms.Button()
+        Me.B6 = New System.Windows.Forms.Button()
+        Me.B4 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.decButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.B0 = New System.Windows.Forms.Button()
+        Me.minusButton = New System.Windows.Forms.Button()
+        Me.equalButton = New System.Windows.Forms.Button()
+        Me.DivButton = New System.Windows.Forms.Button()
+        Me.timesButton = New System.Windows.Forms.Button()
+        Me.plusButton = New System.Windows.Forms.Button()
+        Me.SqrtB = New System.Windows.Forms.Button()
+        Me.piButton = New System.Windows.Forms.Button()
+        Me.TanButton = New System.Windows.Forms.Button()
+        Me.CosButton = New System.Windows.Forms.Button()
+        Me.SinButton = New System.Windows.Forms.Button()
+        Me.MrButton = New System.Windows.Forms.Button()
+        Me.McButton = New System.Windows.Forms.Button()
+        Me.MButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'DisplayTextBox
+        '
+        Me.DisplayTextBox.Location = New System.Drawing.Point(12, 12)
+        Me.DisplayTextBox.Name = "DisplayTextBox"
+        Me.DisplayTextBox.Size = New System.Drawing.Size(249, 22)
+        Me.DisplayTextBox.TabIndex = 0
+        '
+        'B7
+        '
+        Me.B7.Location = New System.Drawing.Point(13, 41)
+        Me.B7.Name = "B7"
+        Me.B7.Size = New System.Drawing.Size(57, 23)
+        Me.B7.TabIndex = 1
+        Me.B7.Text = "7"
+        Me.B7.UseVisualStyleBackColor = True
+        '
+        'B9
+        '
+        Me.B9.Location = New System.Drawing.Point(133, 41)
+        Me.B9.Name = "B9"
+        Me.B9.Size = New System.Drawing.Size(57, 23)
+        Me.B9.TabIndex = 2
+        Me.B9.Text = "9"
+        Me.B9.UseVisualStyleBackColor = True
+        '
+        'B8
+        '
+        Me.B8.Location = New System.Drawing.Point(73, 41)
+        Me.B8.Name = "B8"
+        Me.B8.Size = New System.Drawing.Size(57, 23)
+        Me.B8.TabIndex = 3
+        Me.B8.Text = "8"
+        Me.B8.UseVisualStyleBackColor = True
+        '
+        'B5
+        '
+        Me.B5.Location = New System.Drawing.Point(73, 70)
+        Me.B5.Name = "B5"
+        Me.B5.Size = New System.Drawing.Size(57, 23)
+        Me.B5.TabIndex = 6
+        Me.B5.Text = "5"
+        Me.B5.UseVisualStyleBackColor = True
+        '
+        'B6
+        '
+        Me.B6.Location = New System.Drawing.Point(133, 70)
+        Me.B6.Name = "B6"
+        Me.B6.Size = New System.Drawing.Size(57, 23)
+        Me.B6.TabIndex = 5
+        Me.B6.Text = "6"
+        Me.B6.UseVisualStyleBackColor = True
+        '
+        'B4
+        '
+        Me.B4.Location = New System.Drawing.Point(13, 70)
+        Me.B4.Name = "B4"
+        Me.B4.Size = New System.Drawing.Size(57, 23)
+        Me.B4.TabIndex = 4
+        Me.B4.Text = "4"
+        Me.B4.UseVisualStyleBackColor = True
+        '
+        'B2
+        '
+        Me.B2.AccessibleDescription = ""
+        Me.B2.Location = New System.Drawing.Point(73, 99)
+        Me.B2.Name = "B2"
+        Me.B2.Size = New System.Drawing.Size(57, 23)
+        Me.B2.TabIndex = 9
+        Me.B2.Text = "2"
+        Me.B2.UseVisualStyleBackColor = True
+        '
+        'B3
+        '
+        Me.B3.Location = New System.Drawing.Point(133, 99)
+        Me.B3.Name = "B3"
+        Me.B3.Size = New System.Drawing.Size(57, 23)
+        Me.B3.TabIndex = 8
+        Me.B3.Text = "3"
+        Me.B3.UseVisualStyleBackColor = True
+        '
+        'B1
+        '
+        Me.B1.Location = New System.Drawing.Point(13, 99)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(57, 23)
+        Me.B1.TabIndex = 7
+        Me.B1.Text = "1"
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'decButton
+        '
+        Me.decButton.Location = New System.Drawing.Point(73, 128)
+        Me.decButton.Name = "decButton"
+        Me.decButton.Size = New System.Drawing.Size(57, 23)
+        Me.decButton.TabIndex = 12
+        Me.decButton.Text = "."
+        Me.decButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(133, 128)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(57, 23)
+        Me.ClearButton.TabIndex = 11
+        Me.ClearButton.Text = "C"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'B0
+        '
+        Me.B0.Location = New System.Drawing.Point(13, 128)
+        Me.B0.Name = "B0"
+        Me.B0.Size = New System.Drawing.Size(57, 23)
+        Me.B0.TabIndex = 10
+        Me.B0.Text = "0"
+        Me.B0.UseVisualStyleBackColor = True
+        '
+        'minusButton
+        '
+        Me.minusButton.Location = New System.Drawing.Point(196, 69)
+        Me.minusButton.Name = "minusButton"
+        Me.minusButton.Size = New System.Drawing.Size(23, 24)
+        Me.minusButton.TabIndex = 13
+        Me.minusButton.Text = "-"
+        Me.minusButton.UseVisualStyleBackColor = True
+        '
+        'equalButton
+        '
+        Me.equalButton.Location = New System.Drawing.Point(196, 156)
+        Me.equalButton.Name = "equalButton"
+        Me.equalButton.Size = New System.Drawing.Size(23, 24)
+        Me.equalButton.TabIndex = 14
+        Me.equalButton.Text = "="
+        Me.equalButton.UseVisualStyleBackColor = True
+        '
+        'DivButton
+        '
+        Me.DivButton.Location = New System.Drawing.Point(196, 99)
+        Me.DivButton.Name = "DivButton"
+        Me.DivButton.Size = New System.Drawing.Size(23, 24)
+        Me.DivButton.TabIndex = 15
+        Me.DivButton.Text = "/"
+        Me.DivButton.UseVisualStyleBackColor = True
+        '
+        'timesButton
+        '
+        Me.timesButton.Location = New System.Drawing.Point(196, 127)
+        Me.timesButton.Name = "timesButton"
+        Me.timesButton.Size = New System.Drawing.Size(23, 24)
+        Me.timesButton.TabIndex = 16
+        Me.timesButton.Text = "x"
+        Me.timesButton.UseVisualStyleBackColor = True
+        '
+        'plusButton
+        '
+        Me.plusButton.Location = New System.Drawing.Point(196, 41)
+        Me.plusButton.Name = "plusButton"
+        Me.plusButton.Size = New System.Drawing.Size(23, 24)
+        Me.plusButton.TabIndex = 17
+        Me.plusButton.Text = "+"
+        Me.plusButton.UseVisualStyleBackColor = True
+        '
+        'SqrtB
+        '
+        Me.SqrtB.Location = New System.Drawing.Point(225, 71)
+        Me.SqrtB.Name = "SqrtB"
+        Me.SqrtB.Size = New System.Drawing.Size(36, 23)
+        Me.SqrtB.TabIndex = 18
+        Me.SqrtB.Text = "√ "
+        Me.SqrtB.UseVisualStyleBackColor = True
+        '
+        'piButton
+        '
+        Me.piButton.Location = New System.Drawing.Point(225, 41)
+        Me.piButton.Name = "piButton"
+        Me.piButton.Size = New System.Drawing.Size(36, 25)
+        Me.piButton.TabIndex = 19
+        Me.piButton.Text = "π"
+        Me.piButton.UseVisualStyleBackColor = True
+        '
+        'TanButton
+        '
+        Me.TanButton.Location = New System.Drawing.Point(73, 157)
+        Me.TanButton.Name = "TanButton"
+        Me.TanButton.Size = New System.Drawing.Size(57, 23)
+        Me.TanButton.TabIndex = 20
+        Me.TanButton.Text = "Tan"
+        Me.TanButton.UseVisualStyleBackColor = True
+        '
+        'CosButton
+        '
+        Me.CosButton.Location = New System.Drawing.Point(13, 157)
+        Me.CosButton.Name = "CosButton"
+        Me.CosButton.Size = New System.Drawing.Size(57, 23)
+        Me.CosButton.TabIndex = 21
+        Me.CosButton.Text = "Cos"
+        Me.CosButton.UseVisualStyleBackColor = True
+        '
+        'SinButton
+        '
+        Me.SinButton.Location = New System.Drawing.Point(133, 157)
+        Me.SinButton.Name = "SinButton"
+        Me.SinButton.Size = New System.Drawing.Size(57, 23)
+        Me.SinButton.TabIndex = 22
+        Me.SinButton.Text = "Sin"
+        Me.SinButton.UseVisualStyleBackColor = True
+        '
+        'MrButton
+        '
+        Me.MrButton.Location = New System.Drawing.Point(225, 98)
+        Me.MrButton.Name = "MrButton"
+        Me.MrButton.Size = New System.Drawing.Size(49, 24)
+        Me.MrButton.TabIndex = 23
+        Me.MrButton.Text = "MR"
+        Me.MrButton.UseVisualStyleBackColor = True
+        '
+        'McButton
+        '
+        Me.McButton.Location = New System.Drawing.Point(225, 128)
+        Me.McButton.Name = "McButton"
+        Me.McButton.Size = New System.Drawing.Size(49, 24)
+        Me.McButton.TabIndex = 24
+        Me.McButton.Text = "MC"
+        Me.McButton.UseVisualStyleBackColor = True
+        '
+        'MButton
+        '
+        Me.MButton.Location = New System.Drawing.Point(225, 154)
+        Me.MButton.Name = "MButton"
+        Me.MButton.Size = New System.Drawing.Size(49, 26)
+        Me.MButton.TabIndex = 25
+        Me.MButton.Text = "M+"
+        Me.MButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(298, 186)
+        Me.Controls.Add(Me.MButton)
+        Me.Controls.Add(Me.McButton)
+        Me.Controls.Add(Me.MrButton)
+        Me.Controls.Add(Me.SinButton)
+        Me.Controls.Add(Me.CosButton)
+        Me.Controls.Add(Me.TanButton)
+        Me.Controls.Add(Me.piButton)
+        Me.Controls.Add(Me.SqrtB)
+        Me.Controls.Add(Me.plusButton)
+        Me.Controls.Add(Me.timesButton)
+        Me.Controls.Add(Me.DivButton)
+        Me.Controls.Add(Me.equalButton)
+        Me.Controls.Add(Me.minusButton)
+        Me.Controls.Add(Me.decButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.B0)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.B5)
+        Me.Controls.Add(Me.B6)
+        Me.Controls.Add(Me.B4)
+        Me.Controls.Add(Me.B8)
+        Me.Controls.Add(Me.B9)
+        Me.Controls.Add(Me.B7)
+        Me.Controls.Add(Me.DisplayTextBox)
+        Me.Name = "Form1"
+        Me.Text = "Calc"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DisplayTextBox As TextBox
+    Friend WithEvents B7 As Button
+    Friend WithEvents B9 As Button
+    Friend WithEvents B8 As Button
+    Friend WithEvents B5 As Button
+    Friend WithEvents B6 As Button
+    Friend WithEvents B4 As Button
+    Friend WithEvents B2 As Button
+    Friend WithEvents B3 As Button
+    Friend WithEvents B1 As Button
+    Friend WithEvents decButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents B0 As Button
+    Friend WithEvents minusButton As Button
+    Friend WithEvents equalButton As Button
+    Friend WithEvents DivButton As Button
+    Friend WithEvents timesButton As Button
+    Friend WithEvents plusButton As Button
+    Friend WithEvents SqrtB As Button
+    Friend WithEvents piButton As Button
+    Friend WithEvents TanButton As Button
+    Friend WithEvents CosButton As Button
+    Friend WithEvents SinButton As Button
+    Friend WithEvents MrButton As Button
+    Friend WithEvents McButton As Button
+    Friend WithEvents MButton As Button
+End Class
---
+++ VB/Calculator/Calculator/Form1.vb
@@ -0,0 +1,94 @@
+﻿Public Class Form1
+    Dim first As Decimal
+    Dim second As Decimal
+    Dim oper As String
+    Dim mem As Decimal
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles B2.Click, B9.Click, B8.Click, B7.Click, B6.Click, B5.Click, B4.Click, B3.Click, B1.Click, B0.Click
+        DisplayTextBox.Text += sender.text
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        DisplayTextBox.Clear()
+    End Sub
+
+    Private Sub plusButton_Click(sender As Object, e As EventArgs) Handles plusButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, first)
+        oper = sender.text
+        DisplayTextBox.Clear()
+    End Sub
+
+    Private Sub equalButton_Click(sender As Object, e As EventArgs) Handles equalButton.Click
+        If oper = "+" Then
+            Decimal.TryParse(DisplayTextBox.Text, second)
+            DisplayTextBox.Text = first + second
+        End If
+        If oper = "-" Then
+            Decimal.TryParse(DisplayTextBox.Text, second)
+            DisplayTextBox.Text = first - second
+        End If
+        If oper = "/" Then
+            Decimal.TryParse(DisplayTextBox.Text, second)
+            DisplayTextBox.Text = first / second
+        End If
+        If oper = "x" Then
+            Decimal.TryParse(DisplayTextBox.Text, second)
+            DisplayTextBox.Text = first * second
+        End If
+    End Sub
+
+
+
+    Private Sub DivButton_Click(sender As Object, e As EventArgs) Handles DivButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, first)
+        oper = sender.text
+        DisplayTextBox.Clear()
+    End Sub
+
+    Private Sub timesButton_Click(sender As Object, e As EventArgs) Handles timesButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, first)
+        oper = sender.text
+        DisplayTextBox.Clear()
+    End Sub
+
+    Private Sub minusButton_Click(sender As Object, e As EventArgs) Handles minusButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, first)
+        oper = sender.text
+        DisplayTextBox.Clear()
+    End Sub
+
+    Private Sub SqrtB_Click(sender As Object, e As EventArgs) Handles SqrtB.Click
+        Decimal.TryParse(DisplayTextBox.Text, first)
+        DisplayTextBox.Text = Math.Sqrt(first)
+    End Sub
+
+    Private Sub piButton_Click(sender As Object, e As EventArgs) Handles piButton.Click
+        DisplayTextBox.Text = Math.PI
+    End Sub
+
+    Private Sub CosButton_Click(sender As Object, e As EventArgs) Handles CosButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, first)
+        DisplayTextBox.Text = Math.Cos(first)
+    End Sub
+
+    Private Sub TanButton_Click(sender As Object, e As EventArgs) Handles TanButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, first)
+        DisplayTextBox.Text = Math.Tan(first)
+    End Sub
+
+    Private Sub SinButton_Click(sender As Object, e As EventArgs) Handles SinButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, first)
+        DisplayTextBox.Text = Math.Sin(first)
+    End Sub
+
+    Private Sub MButton_Click(sender As Object, e As EventArgs) Handles MButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, mem)
+    End Sub
+
+    Private Sub McButton_Click(sender As Object, e As EventArgs) Handles McButton.Click
+        mem = 0
+    End Sub
+
+    Private Sub MrButton_Click(sender As Object, e As EventArgs) Handles MrButton.Click
+        DisplayTextBox.Text = mem
+    End Sub
+End Class
